37ba6f5781be2c7c048a978468ff9c74a33747fc
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-11-10 22:42:02 -0600

    Subida de archivos a cloudinary y ajustes para obtener la imagen

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,18 +1,19 @@
 import { Product } from "@/src/schemas";
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, getImagePath, isAvailable } from "@/src/utils";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
 
 
-export default function ProductCard({product} : {product : Product}) {
+export default function ProductCard({ product }: { product: Product }) {
     return (
         <div
             className='rounded bg-white shadow relative p-5'
         >
-            <div>
-
-                <Image 
-                    src={`${process.env.API_URL}/img/${product.image}`}
+            <div
+                className={`${!isAvailable(product.inventory) && 'opacity-40'}`}
+            >
+                <Image
+                    src={getImagePath(product.image)}
                     alt={`Imagen de Producto ${product.name}`}
                     width={400}
                     height={600}
@@ -26,9 +27,15 @@ export default function ProductCard({product} : {product : Product}) {
                     <p className="text-2xl font-extrabold  text-gray-900"> {formatCurrency(product.price)} </p>
                 </div>
             </div>
-            <AddProductButton 
-                product={product}
-            />
+            {
+                isAvailable(product.inventory) ? (
+                    <AddProductButton
+                        product={product}
+                    />
+                ) : (
+                    <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white opacity-60 w-full text-center py-5 text-2xl uppercase font-black">Agotado</p>
+                )
+            }
         </div>
     )
 }
